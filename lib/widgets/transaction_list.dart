ffc086da5fd864f2bfb4613c9d8770f7f13ba7bd
Manorama <manoramamaharana@gmail.com>
2020-04-17 12:33:28 +0530

    initial commit

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,54 @@
+import '../models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList  extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: ListView(
+            children: transactions.map((tx) {
+            return Card(
+              child : Row(
+                children : <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                margin:EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 15,
+                                ),
+                                decoration:BoxDecoration(border: Border.all(
+                                                              color:Colors.purple,
+                                                              width:2,
+                                                              )
+                                                              ),
+                                child : Text(
+                                '₹${tx.amount}',
+                                style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                            color: Colors.purple,
+                                            )
+                                            ),
+                              ),
+
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children : <Widget>[
+                                  Text(tx.title, style :TextStyle(fontSize: 18,fontWeight: FontWeight.bold)),
+                                  Text(
+                                    DateFormat.yMMMMd().format(tx.date),
+                                    style: TextStyle(color: Colors.grey,)),
+                                ])
+              ])
+              );
+          }).toList(),
+          ),
+    );
+    }
+}
