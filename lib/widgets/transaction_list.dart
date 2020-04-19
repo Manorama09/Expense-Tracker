4c26445499f9bc9622f1a96e8e368c342c5e3fda
Manorama <manoramamaharana@gmail.com>
2020-04-19 21:07:34 +0530

    added chart feature

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,54 +1,50 @@
-import '../models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import '../models/transaction.dart';
 
-class TransactionList  extends StatelessWidget {
+class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-
   TransactionList(this.transactions);
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
-      child: ListView(
-            children: transactions.map((tx) {
-            return Card(
-              child : Row(
-                children : <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                margin:EdgeInsets.symmetric(
-                                  vertical: 10,
-                                  horizontal: 15,
-                                ),
-                                decoration:BoxDecoration(border: Border.all(
-                                                              color:Colors.purple,
-                                                              width:2,
-                                                              )
-                                                              ),
-                                child : Text(
-                                '₹${tx.amount}',
-                                style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20,
-                                            color: Colors.purple,
-                                            )
-                                            ),
-                              ),
-
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children : <Widget>[
-                                  Text(tx.title, style :TextStyle(fontSize: 18,fontWeight: FontWeight.bold)),
-                                  Text(
-                                    DateFormat.yMMMMd().format(tx.date),
-                                    style: TextStyle(color: Colors.grey,)),
-                                ])
-              ])
-              );
-          }).toList(),
-          ),
+      height: 300,
+      child: transactions.isEmpty? Column(children:<Widget>[
+        Text('No transaction added yet!',
+        style: Theme.of(context).textTheme.title,
+        ),
+        SizedBox(height:10),
+        Container(
+        height:200 ,
+        child: Image.asset('assets/images/design.png', fit:BoxFit.cover),
+        ),
+        ],
+      )
+      :ListView.builder(
+        itemBuilder: (ctx, index) {
+         return Card(
+           margin:EdgeInsets.symmetric(vertical: 8, horizontal:5) ,
+           elevation: 8,
+           child: ListTile(
+           leading: CircleAvatar(radius: 30,
+           child: Padding(
+             padding: EdgeInsets.all(6),
+             child: FittedBox(
+             child: Text('₹${transactions[index].amount}'),
+             ),
+           ),
+         ),
+         title: Text(transactions[index].title,
+         style: Theme.of(context).textTheme.title,
+         ),
+         subtitle: Text(DateFormat.yMMMd().format(transactions[index].date),
+         ),
+           ),
+         );
+        },
+        itemCount: transactions.length,
+      ),
     );
-    }
+  }
 }
